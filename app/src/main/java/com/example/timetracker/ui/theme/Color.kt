96e299de567da759f4c6128eccd42dbe233f9611
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-19 17:51:22 +0200

    Add Color

---
+++ app/src/main/java/com/example/timetracker/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.timetracker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BluePrimaryLight = Color(0xFF0028FF)
+val OnBlueLight = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFEAEAEA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFF3F3F3)
+val LightOnSurface = Color(0xFF000000)
+val LightOnSurfaceVariant = Color(0xFF808080)
+
+
+val BluePrimaryDark = Color(0xFF4C68FF)
+val OnBlueDark = Color(0xFF000000)
+val DarkBackground = Color(0xFF212121)
+val DarkSurface = Color(0xFF414141)
+val DarkOnSurface = Color(0xFFFFFFFF)
+val DarkSurfaceVariant = Color(0xFF2A2A2A)
+val DarkOnSurfaceVariant = Color(0xFFB9B9B9)
